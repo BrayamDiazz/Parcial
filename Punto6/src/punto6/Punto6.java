f14c77c52517c5b27d8a997dc38e3907c47064e9
franco burbano <franco.009@hotmail.com>
2013-08-30 08:51:51 -0500

    punto 6

---
+++ Punto6/src/punto6/Punto6.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package punto6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 202
+ */
+public class Punto6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+        int x;
+        String vocal;
+        System.out.println("Escriba una letra");
+        vocal = entrada.next();
+        if(vocal.equals("A")|| vocal.equals("E")){
+            return;
+            
+        } else {
+        }
+        x =0;
+        System.out.println(x);
+    }
+}
---
+++ Punto6/test/punto6/Punto6Test.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package punto6;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 202
+ */
+public class Punto6Test {
+    
+    public Punto6Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class Punto6.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        Punto6.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
